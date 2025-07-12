0b9321ea7dcb53acc13ee89f919e3d52f4a08b4b
Abhishek1017-sh <as20041009@gmail.com>
2025-07-12 13:04:36 +0000

    assignment_55.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import ProductCard from './ProductCard';
+import Details from './moreDetails';
 
 function App() {
+  const [selectedItem, setSelectedItem] = useState(null);
+  const handleShowDetails = (item) => {
+    setSelectedItem(item);
+  };
+  const handleGoBack = () => {
+    setSelectedItem(null);
+  };
   return (
-    <>
-      <div>
+      <div className='bg-gray-300 min-h-screen'>
+        {selectedItem ? (
+        <Details item={selectedItem} goBack={handleGoBack} />
+        ):(
+          <>
         <Header />
         <div className="bg-white m-12 rounded-lg shadow-lg p-6">
         <div className="bg-white m-12 mb-0 flex justify-end ">
@@ -23,54 +34,63 @@ function App() {
             title="Black Printed Coffee Mug"
             category="Mugs"
             price="15.00"
+            moreDetails={handleShowDetails}
             />
           <ProductCard
             image="https://m.media-amazon.com/images/I/61rc196ZwUL._UF894,1000_QL80_.jpg"
             title="Father's day coffee mug"
             category="Mugs"
             price="19.00"
+            moreDetails={handleShowDetails}
           />
           <ProductCard
             image="https://putra.in/cdn/shop/files/IMG-0022_1200x.jpg?v=1732647723"
             title="Green Printed Tshirt"
             category="Tshirts"
             price="34.00"
+            moreDetails={handleShowDetails}
           />
           <ProductCard
             image="https://betweenboxes.in/cdn/shop/files/cust-mug-600x600-500x500.webp?v=1720606037"
             title="Personalised mug"
             category="Mugs"
             price="15.00"
+            moreDetails={handleShowDetails}
           />
           <ProductCard
             image="https://www.botnia.in/cdn/shop/files/47_b87415d3-951b-4172-aa68-89d63ec8f5f0_800x.png?v=1696434833"
             title="Printed Brown Tshirts"
             category="Tshirts"
             price="25.00"
+            moreDetails={handleShowDetails}
           />
           <ProductCard
             image="https://www.rockit.co.in/cdn/shop/files/2240101920-1-38_1.jpg?v=1715680470"
             title="Printed Dark Blue Tshirts"
             category="Tshirts"
             price="34.00"
+            moreDetails={handleShowDetails}
           />
           <ProductCard
             image="https://muselot.in/cdn/shop/products/Never-give-up-1.jpg?v=1644528299"
             title="Printed Green tshirts"
             category="Tshirts"
             price="28.00"
+            moreDetails={handleShowDetails}
           />
           <ProductCard
             image="https://muselot.in/cdn/shop/products/I-am-coffee-hot4.jpg?v=1625002565"
             title="Printed Tshirt Coffee Black Color"
             category="Tshirts"
             price="25.00"
+            moreDetails={handleShowDetails}
           />
           <ProductCard
             image="https://images.meesho.com/images/products/248447273/hlsee_512.webp"
             title="Typography Teal Printed Tshirt"
             category="Tshirts"
             price="32.00"
+            moreDetails={handleShowDetails}
           />
 
         </div>
@@ -81,10 +101,10 @@ function App() {
         </div>
         </div>
         <Footer />
+        </>
+      )}
       </div>
-
-    </>
-  );
+  )
 }
 
 export default App;
